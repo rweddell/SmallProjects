70d50499de93e2230b7382e559e821b23482d4a0
Rob Weddell <rweddell4@gmail.com>
2022-04-02 11:18:17 -0500

    Updates to gitignore

--- Mazes/AsciiMaskDemo.py
+++ Mazes/AsciiMaskDemo.py
@@ -5,7 +5,7 @@ from Algorithms.RecursiveBacktracker import RecursiveBacktracker
 
 recback = RecursiveBacktracker()
 
-mask = from_txt('Output\masktext.txt')
+mask = from_txt('.Output\masktext.txt')
 grid = recback.on(MaskedGrid(mask))
 img = grid.to_png()
 img.save('Output\mascii.png')
